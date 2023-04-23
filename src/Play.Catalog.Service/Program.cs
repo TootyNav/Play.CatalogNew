c8c355a326a000b0affea04994e66c002eec4c81
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2023-04-23 18:40:50 +0100

    Make extensions for mongodb for program class

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -8,33 +8,14 @@ using Play.Catalog.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.COnnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-    {
-        //you have to pass on databse  class implementation to mongorepository 
-        //this is how you get it from the service provider.
-        var database = serviceProvider.GetService<IMongoDatabase>();
-        return new MongoRepository<Item>(database, "Items");
-    }
-);
-
-//Change guid and date to readable string in mango db
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
 builder.Services.AddControllers(options =>
 {
     //methods with async in their name are changedon run time. we need this to use nameof in controllers
     options.SuppressAsyncSuffixInActionNames = false;
 });
+
+builder.Services.AddMongo()
+    .AddMongoRepository<Item>("items");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/Play.Catalog.Service/Repositories/Extensions.cs
@@ -0,0 +1,43 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Settings;
+
+namespace Play.Catalog.Service.Repositories;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMongo(this IServiceCollection services)
+    {
+
+        //Change guid and date to readable string in mango db
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+        services.AddSingleton(serviceProvider =>
+        {
+            var configuration = serviceProvider.GetService<IConfiguration>(); // get config from service provider
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            var mongoClient = new MongoClient(mongoDbSettings.COnnectionString);
+            return mongoClient.GetDatabase(serviceSettings.ServiceName);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collectionName) where T : IEntity
+    {
+        services.AddSingleton<IRepository<T>>(serviceProvider =>
+        {
+            //you have to pass on databse  class implementation to mongorepository 
+            //this is how you get it from the service provider.
+            var database = serviceProvider.GetService<IMongoDatabase>();
+            return new MongoRepository<T>(database, collectionName);
+        }
+    );
+        return services;
+    }
+}
